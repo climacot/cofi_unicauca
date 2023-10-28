56302532c45a39ca3667a6285fb1eeaf23231f4b
climacot <fermt111@gmail.com>
2023-10-28 18:57:37 -0500

    initial layout home

--- app/src/main/java/com/unicauca/cofi/views/HomeView.kt
+++ app/src/main/java/com/unicauca/cofi/views/HomeView.kt
@@ -1,18 +1,33 @@
 package com.unicauca.cofi.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.unicauca.cofi.R
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,10 +38,29 @@ fun HomeView(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = {
-                    Text("Top app bar")
-                }
+                    Image(
+                        painter = painterResource(id = R.drawable.cofi_appbar),
+                        contentDescription = "Logo cofi"
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Outlined.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Outlined.AccountCircle,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
             )
         },
         content = { paddingValues ->
@@ -37,12 +71,32 @@ fun HomeView(
             )
         },
         bottomBar = {
-            BottomAppBar {
-                Text(
-                    modifier = Modifier.fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = "Bottom app bar",
+            BottomAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(
+                    horizontal = 50.dp
                 )
+            ) {
+                IconButton(onClick = {}) {
+                    Icon(
+                        Icons.Outlined.Home,
+                        contentDescription = "Ir a inicio"
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(onClick = {}) {
+                    Icon(
+                        Icons.Outlined.Info,
+                        contentDescription = "Ir a inicio"
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                IconButton(onClick = {}) {
+                    Icon(
+                        Icons.Outlined.CheckCircle,
+                        contentDescription = "Ir a inicio"
+                    )
+                }
             }
         },
     )
@@ -56,6 +110,11 @@ fun Content(
     Column(
         modifier = Modifier.padding(paddingValues)
     ) {
+        Text(
+            text = "Mi finca",
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Normal
+        )
         Button(onClick = onLogout) {
             Text(text = "Cerrar sesión")
 
@@ -64,4 +123,13 @@ fun Content(
             Text(text = "Registrar kilos recolectados")
         }
     }
+}
+
+@Preview
+@Composable
+fun HomePreview() {
+    HomeView(
+        goToRegister = {},
+        onLogout = {}
+    )
 }
