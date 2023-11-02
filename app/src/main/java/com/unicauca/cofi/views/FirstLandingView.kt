a81867eb5e316e3426311efef61288f1950b8378
cristiancast <cristiancast@unicauca.edu.co>
2023-11-02 02:20:03 -0500

    FirstLandingView Succesful

--- app/src/main/java/com/unicauca/cofi/views/FirstLandingView.kt
+++ app/src/main/java/com/unicauca/cofi/views/FirstLandingView.kt
@@ -1,16 +1,120 @@
 package com.unicauca.cofi.views
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.unicauca.cofi.R
 
 @Composable
 fun FirstLandingView(goToSecondLanding: () -> Unit) {
-    Column {
-        Text(text = "Ahorra tiempo calculando automáticamente el pago de tus trabajadores.")
-        Button(onClick = goToSecondLanding) {
-            Text("Continuar")
+    var bgColor = Color(0xFFF8FAF6)
+    var primaryColor = Color(0xFF4FA884)
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(bgColor)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(modifier = Modifier.fillMaxWidth(), Alignment.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.background_large),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillBounds,
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.landing_page_1),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillBounds,
+                )
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Ahorra tiempo calculando automáticamente el pago de tus trabajadores.",
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.width(240.dp)
+                )
+                Spacer(modifier = Modifier.height(100.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(modifier = Modifier.width(104.dp))
+                    Row(
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .size(size = 12.dp)
+                                .clip(shape = CircleShape)
+                                .background(Color(0xFFDDC088))
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Box(
+                            modifier = Modifier
+                                .size(size = 12.dp)
+                                .clip(shape = CircleShape)
+                                .background(Color(0xFFF0EBDB))
+                        )
+                    }
+                    TextButton(
+                        onClick = goToSecondLanding,
+                    ) {
+                        Text(
+                            text = "Continuar",
+                            color = primaryColor,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                }
+            }
         }
     }
+}
+
+@Preview
+@Composable
+fun prevFirstLanding() {
+    FirstLandingView {}
 }
